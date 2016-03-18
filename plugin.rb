b6b11ffbbf9f6b1e912856d250e360c67d85f89a
Buzz <davidbuzz@gmail.com>
2016-03-18 15:25:43 +1000

    v 0.3 supports logoff

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse cookie domain setup
 # about: modifications for forums and stuff
-# version: 0.2
+# version: 0.3
 # authors: ntauthority,davidbuzz
 
 load File.expand_path("../current_user_provider.rb", __FILE__)
